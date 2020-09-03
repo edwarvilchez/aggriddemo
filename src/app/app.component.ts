d4894ce622cc1f4daf1886f822745569bc40a5b2
edwarvilchez <edwarvilchez1977@gmail.com>
2020-09-03 11:02:14 -0400

    Se crea el proyecto y se agregan las dependencias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'agGridDemo';
+
+// apertura del area para uso de ag-grid
+  columnDefs = [
+        {headerName: 'Marca', field: 'marca' },
+        {headerName: 'Modelo', field: 'modelo' },
+        {headerName: 'Precio', field: 'precio'}
+    ];
+
+    rowData = [
+        {	marca: 'Toyota', modelo: 'Celica', precio: 35000 },
+        { 	marca: 'Ford', modelo: 'Mondeo', precio: 32000 },
+        { 	marca: 'Porsche', modelo: 'Boxter', precio: 72000 }
+    ];
+// cierre del area para uso de ag-grid
 }
