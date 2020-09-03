4e429d8822b351320d085c93258fc4d79ca02531
edwarvilchez <edwarvilchez1977@gmail.com>
2020-09-03 11:25:07 -0400

    Se realizan ajustes a la grilla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AgGridModule } from 'ag-grid-angular';
+import { AgGridModule } from 'ag-grid-angular';//agregamos AgGridModule
 
 @NgModule({
   declarations: [
